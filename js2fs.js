e8d8b68b6c2fc20a9361551836148d80c1bb0c34
Ingo Fischer <github@fischer-ka.de>
2017-09-04 12:17:30 +0200

    we only need to create settings once

--- js2fs.js
+++ js2fs.js
@@ -347,6 +347,7 @@ let Scripts = function () {
 
         source = source.toString();
         if (!obj || obj.isSettings === 'create') {  // create new file
+            obj.isSettings = true;
             return this.create (fn, source, mtime, callback);
         }
 
