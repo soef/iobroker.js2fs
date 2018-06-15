ba81c343cb50fdd38e512f869d4dfa931f7aec05
Ingo Fischer <github@fischer-ka.de>
2018-06-15 20:50:49 +0200

    anothr try

--- js2fs.js
+++ js2fs.js
@@ -437,8 +437,8 @@ let Scripts = function () {
                 else o.ts = mtime*1000;
             obj.common.source = source;
             obj.ts = o.ts;
-            delete obj.mtime;
-            delete o.common.mtime;
+            obj.mtime = 0;
+            o.common.mtime = 0;
             if (adapter.config.restartScript) {
                 oldEnabled = o.common.enabled;
                 o.common.enabled = false;
